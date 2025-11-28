3327ae50104cd8471f6903ef9b6e6ee45b32fe1b
Martin Visuara <martinvisuara10@gmail.com>
2025-11-28 20:30:59 -0300

    Updates

---
+++ footer.js
@@ -0,0 +1,8 @@
+export function footerComponent() {
+    const footer = document.createElement("footer");
+    footer.innerHTML = "<p>© 2025 Mi Portfolio</p>";
+    footer.style.border = "solid 3px black";
+    footer.style.padding = "12px";
+    footer.style.textAlign = "center";
+    return footer;
+}
---
+++ products.js
@@ -0,0 +1,16 @@
+import { getProductsSortedByPrice } from "./products-model.js";
+export function productsListComponent() {
+    const section = document.createElement("section");
+    section.style.border = "solid 1px #ccc";
+    section.style.padding = "10px";
+    const list = document.createElement("ul");
+    // Usamos la nueva función para obtener los productos ordenados
+    const sortedProducts = getProductsSortedByPrice();
+    sortedProducts.forEach((product) => {
+        const listItem = document.createElement("li");
+        listItem.textContent = `${product.title} - $${product.price}`;
+        list.appendChild(listItem);
+    });
+    section.appendChild(list);
+    return section;
+}
